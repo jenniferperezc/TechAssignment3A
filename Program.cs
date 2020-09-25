29d4c321816e4b3bb301b7785a5ae26b838861c8
Jennifer Perez Cabrera <perezcabrera@usf.edu>
2020-09-24 22:33:36 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ 
+Project by: Jennifer Perez Cabrera
+
+*/
+
+using System;
+
+namespace TechAssignment3A
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            
+            
+                Console.WriteLine("Please enter you name..."); // This will gather user input
+                string name = Console.ReadLine();
+                Console.WriteLine();                           // Line break
+
+                //Response
+                Console.WriteLine("Hello!" + name +" " + "go bulls!");
+
+    
+
+        }// End of Main      
+
+    }// End of class
+}// Ens of namespace
